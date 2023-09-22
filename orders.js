b181e8c95a013902f8c5e63ebfd0c6c9afa41e25
Chris Emmanuel I <OKHANIEMMANUEL7@GMAIL.COM>
2023-09-22 19:36:00 +0100

    first commit

---
+++ orders.js
@@ -0,0 +1,38 @@
+const orders = [
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "88882",
+    paymentStatus: "Paid",
+    shippingStatus: "Delivered",
+  },
+  {
+    productName: " HP wireless Mouse",
+    productNumber: "1000182",
+    paymentStatus: "Refund",
+    shippingStatus: "Declined",
+  },
+  {
+    productName: " Dell-Laptop Bag",
+    productNumber: "83082",
+    paymentStatus: "Due",
+    shippingStatus: "Pending",
+  },
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "88200",
+    paymentStatus: "Paid",
+    shippingStatus: "Delivered",
+  },
+  {
+    productName: "DJI Air 30",
+    productNumber: "88882",
+    paymentStatus: "Paid",
+    shippingStatus: "Delivered",
+  },
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "18802",
+    paymentStatus: "Refund",
+    shippingStatus: "Declined",
+  },
+];
